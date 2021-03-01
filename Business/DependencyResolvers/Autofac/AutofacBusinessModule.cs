4368f67d995dfa47ee1f24836c3a87821fea16ff
alanabd <alanabdullah2001@hotmail.com>
2021-03-02 02:06:03 +0300

    autofac

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarServices>().SingleInstance();//burada sistem eğer IProductService isterse ProductManager ver demek istriyoruz
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();//burada sistem eğer IProductService isterse ProductManager ver demek istriyoruz
+        }
+    }
+}
