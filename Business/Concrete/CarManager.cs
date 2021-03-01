7c64586459ff44974ac4dcd69e27665ec058f7f0
alanabd <alanabdullah2001@hotmail.com>
2021-03-02 02:54:41 +0300

    fluentValidation

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 
 namespace Business.Concrete
 {
@@ -21,10 +24,7 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.Description.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            ValidationTool.Validate(new CarValidator(),car);
             _carDal.Add(car);
             return new SuccessResult(Messages.Added);
             
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Description).MinimumLength(2);
+            RuleFor(c => c.Description).NotEmpty();
+        }
+    }
+}
